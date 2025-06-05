1ded8cf006ecbdd6d679156e322c4ef5aa7cb5c4
Nguyễn Trần Phi Hoàng <nguyentranphihoang2005@gmail.com>
2025-06-05 12:58:22 +0700

    tạo gui dangnhap

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,33 @@
 package main;
 
-public class Main {
-	public static void main(String[] args) {
-		
-	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/DangNhap.fxml"));
+            Scene scene = new Scene(root);
+            Stage loginStage = new Stage(); // Mở stage mới cho login
+            loginStage.setTitle("Màn Hình Đăng Nhập");
+            loginStage.setScene(scene);
+            
+            loginStage.setResizable(false);
+            loginStage.initStyle(StageStyle.UNDECORATED);
+            loginStage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
